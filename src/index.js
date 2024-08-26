9f7c9cfeba64e79a2ab8e9f26c3bcc009fc35760
shounak03 <shounakmis.3@gmail.com>
2024-08-26 20:58:25 +0530

    utils files created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 
+import { error } from "console";
 import connectDB from "./db/index.js";
-
-
 import { configDotenv } from "dotenv";
-configDotenv({path:'. /env'})
+import { app } from "./app.js";
+configDotenv({path:'./env'})
+
+
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server running at ${process.env.PORT}`);
+        
+    })
+}).catch((err)=>{
+    console.log("MONGOBD connection error",err);
+    
+})
